a98fa38f2c328568dcb6e59c39674b47b9fbc9ed
zakaria <zakaria@gmail.com>
2024-06-30 20:20:45 +0100

    dfg

--- rendu/ft_strcpy/ft_strcpy.c
+++ rendu/ft_strcpy/ft_strcpy.c
@@ -2,10 +2,10 @@
 
 char    *ft_strcpy(char *s1, char *s2){
 	int i = 0;
-	while(s1[i]){
-		s2[i] = s1[i];
+	while(s2[i]){
+		s1[i] = s2[i];
 		i++;
 	}
-	s2[i] = '\0';
-	return s2;
+	s1[i] = '\0';
+	return s1;
 }
